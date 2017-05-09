6c11dbf0d5b74724034104474d12727dbfb7dfa6
Aaron Teague <aaron.teague@outlook.com>
2017-05-09 16:20:32 -0500

    WIP on scoring system

--- beerApp/src/pages/profile/profile.ts
+++ beerApp/src/pages/profile/profile.ts
@@ -7,13 +7,20 @@ import { DataService } from '../../providers/data-service';
 
 import { EditProfilePagePage } from '../edit-profile-page/edit-profile-page';
 
-
+const WHITE = { low: 0, high: 4900 };
+const BLUE = { low: 5000, high: 19900 };
+const BRONZE = {low: 20000, high: 49900 };
+const SILVER = { low: 50000, high: 149900 };
+const GOLD = { low: 150000, high: 499900 };
+const PLATINUM = { low: 500000, high: -1 }
 
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
+  
+  
 
   //auth: any = null;
   userProfile: any = null;
@@ -31,7 +38,7 @@ export class ProfilePage {
   ionViewDidLoad() {
     this.userProfile = this.dataService.getUser();
     //console.log(this.userProfile);
-    this.dataService.getUserAdditionalData().then(e => this.userExtraData = e.val());
+    this.dataService.getUserAdditionalData().then(e => {this.userExtraData = e.val(); console.log(this.userExtraData)});
     
   }
 
@@ -55,7 +62,26 @@ export class ProfilePage {
   }
 
   getLevel(): string{
-    return "blue";
+    
+
+     let points = this.userExtraData;
+     console.log(points);
+    /*
+    if(points >= WHITE.low && points <= WHITE.high)
+      return "White";
+      
+    else if(points >= BLUE.low && points <= BLUE.high)
+      return "Blue";
+    else if(points >= SILVER.low && points <= SILVER.high)
+      return "Silver";
+    else if(points >= GOLD.low && points <= GOLD.high)
+      return "Gold";
+    else if(points >= PLATINUM.low)
+      return "Platinum";
+    else
+      return "Invalid";
+      */
+      return "herp derp";
   }
 
 }
